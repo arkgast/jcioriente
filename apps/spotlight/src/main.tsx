c66a834d215dab04670124384da0fb5b640a2f3d
Arnold Gandarillas Castillo <arkgast@gmail.com>
2023-08-27 10:51:11 -0400

    chore: add tailwind and react-router-dom

--- apps/spotlight/src/main.tsx
+++ apps/spotlight/src/main.tsx
@@ -1,13 +1,46 @@
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
+import { Link, Outlet, RouterProvider, createBrowserRouter } from 'react-router-dom';
 
-import App from './app/app';
+const App = () => {
+  return (
+    <>
+      <header>
+        <div className="flex justify-end">
+          <Link className="mx-2 p-2 hover:bg-blue-200" to='/'>Inicio</Link>
+          <Link className="mx-2 p-2 hover:bg-blue-400" to='/proyectos'>Proyectos</Link>
+        </div>
+      </header>
+      <main>
+        <h1>JCI Oriente</h1>
+      </main>
+      <Outlet />
+    </>
+  )
+}
+
+const routes = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: '/',
+        element: <h1>Inicio</h1>
+      },
+      {
+        path: '/proyectos',
+        element: <h1>Proyectos</h1>
+      }
+    ]
+  }
+])
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <StrictMode>
-    <App />
+    <RouterProvider router={routes} />
   </StrictMode>
 );
---
+++ apps/spotlight/tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
+const { createGlobPatternsForDependencies } = require('@nx/react/tailwind');
+const { join } = require('path');
+
+module.exports = {
+  content: [
+    join(
+      __dirname,
+      '{src,pages,components}/**/*!(*.stories|*.spec).{ts,tsx,html}'
+    ),
+    ...createGlobPatternsForDependencies(__dirname),
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+};
