c1c25b37dff209be2195f0d66208fffd797d7df7
Arnold Gandarillas Castillo <arkgast@gmail.com>
2023-08-31 17:41:25 -0400

    refactor: use data api to create memory router

--- apps/spotlight/src/test/render.tsx
+++ apps/spotlight/src/test/render.tsx
@@ -1,13 +1,18 @@
 import { RenderResult, render } from '@testing-library/react';
 import { ReactElement } from 'react';
-import { MemoryRouter } from 'react-router-dom';
+import { MemoryRouter, RouterProvider, createMemoryRouter } from 'react-router-dom';
 
 export function renderWithRouter(ui: ReactElement, { route = '/' } = {}): RenderResult {
-  return render(
-    <MemoryRouter
-      initialEntries={[route]}
-    >
-      {ui}
-    </MemoryRouter>
-  );
+  const routes = [
+    {
+      path: route,
+      element: ui,
+    }
+  ]
+
+  const router = createMemoryRouter(routes, {
+    initialEntries: [route]
+  })
+
+  return render(<RouterProvider router={router} />);
 };
