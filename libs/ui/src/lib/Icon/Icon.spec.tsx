a7e76c96cfda30590bdb8400763fab5c2cbb7057
Arnold Gandarillas Castillo <arkgast@gmail.com>
2023-09-03 20:41:55 -0400

    refactor: improve Icon component

--- libs/ui/src/lib/Icon/Icon.spec.tsx
+++ libs/ui/src/lib/Icon/Icon.spec.tsx
@@ -4,7 +4,7 @@ import { Icon } from './Icon';
 
 describe('Icon', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(<Icon />);
+    const { baseElement } = render(<Icon name="tiktok" />);
     expect(baseElement).toBeTruthy();
   });
 });
