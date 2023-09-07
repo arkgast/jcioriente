922ad5b03d52ffd2522dc64938c2bca609777db8
Arnold Gandarillas Castillo <arkgast@gmail.com>
2023-09-07 15:46:49 -0400

    feat: add form validation

--- apps/spotlight/src/pages/Login.tsx
+++ apps/spotlight/src/pages/Login.tsx
@@ -1,6 +1,34 @@
 import { Button, Input } from '@jcioriente/ui';
+import { useForm, SubmitHandler } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { z } from 'zod';
+
+const schema = z.object({
+  email: z.string().email({ message: 'Correo electrónico inválido.' }),
+  password: z
+    .string()
+    .min(6, { message: 'La contraseña debe tener al menos 6 caracteres.' }),
+});
+
+type Inputs = {
+  email: string;
+  password: string;
+};
 
 export function Login() {
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    formState: { errors },
+  } = useForm<Inputs>({ resolver: zodResolver(schema) });
+
+  const onSubmit: SubmitHandler<Inputs> = (data: Inputs) => {
+    console.log({ data });
+    setValue('email', '');
+    setValue('password', '');
+  };
+
   return (
     <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-sm">
@@ -10,14 +38,21 @@ export function Login() {
       </div>
 
       <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-        <form className="space-y-6" action="#" method="POST">
+        <form
+          noValidate
+          className="space-y-6"
+          onSubmit={handleSubmit(onSubmit)}
+        >
           <div>
             <Input
               label="Correo electrónico"
               id="email"
-              name="email"
               type="email"
               autoComplete="email"
+              error={!!errors.email}
+              helperText={errors.email?.message}
+              aria-invalid={errors.email ? 'true' : 'false'}
+              {...register('email')}
             />
           </div>
 
@@ -25,9 +60,12 @@ export function Login() {
             <Input
               label="Contraseña"
               id="password"
-              name="password"
               type="password"
               autoComplete="current-password"
+              error={!!errors.password}
+              helperText={errors.password?.message}
+              aria-invalid={errors.password ? 'true' : 'false'}
+              {...register('password')}
             />
           </div>
 
